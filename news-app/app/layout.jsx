da03cb6b8211109c688f18d5fa691e2514e8e708
gamolong <gaille.amolong1@gmail.com>
2024-04-27 00:59:01 +0800

    added news route

---
+++ news-app/app/layout.jsx
@@ -0,0 +1,18 @@
+import MainHeader from '@/components/main-header';
+import './globals.css';
+
+export const metadata = {
+  title: 'News Web app',
+  description: 'News Web application',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <MainHeader />
+        {children}
+      </body>
+    </html>
+  );
+}
